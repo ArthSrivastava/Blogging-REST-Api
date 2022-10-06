0bc8662d1363579981447ca018e80289b0112191
Arth <arth4701@gmail.com>
2022-10-06 22:58:37 +0530

    completed the comment api

---
+++ src/main/java/com/thesnoozingturtle/bloggingrestapi/controllers/CommentController.java
@@ -0,0 +1,31 @@
+package com.thesnoozingturtle.bloggingrestapi.controllers;
+
+import com.thesnoozingturtle.bloggingrestapi.payloads.ApiResponse;
+import com.thesnoozingturtle.bloggingrestapi.payloads.CommentDto;
+import com.thesnoozingturtle.bloggingrestapi.services.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/user/{userId}/post/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@RequestBody CommentDto commentDto,
+                                                    @PathVariable int userId, @PathVariable int postId) {
+        CommentDto comment = this.commentService.createComment(commentDto, postId, userId);
+        return new ResponseEntity<>(comment, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public ResponseEntity<ApiResponse> deleteComment(@PathVariable int commentId) {
+        this.commentService.deleteComment(commentId);
+        return new ResponseEntity<>(new ApiResponse("Comment deleted successfully!", true), HttpStatus.OK);
+    }
+
+}
