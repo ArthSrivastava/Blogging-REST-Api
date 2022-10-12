107dbfdbcd9587eb0d1e64e9d2d4c6f94e266450
Arth <arth4701@gmail.com>
2022-10-12 17:42:39 +0530

    removed some unnecessary imports

--- src/main/java/com/thesnoozingturtle/bloggingrestapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/thesnoozingturtle/bloggingrestapi/exceptions/GlobalExceptionHandler.java
@@ -5,12 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
